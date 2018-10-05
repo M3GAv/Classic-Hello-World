f1d15b406632b417c6387e67d811c600c1102c44
M3GAv <mateuszcelka@wp.pl>
2018-10-05 18:48:54 +0200

    Added credits

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            credits();
             Console.WriteLine("Hello World!");
+
+        }
+
+        static void credits()
+        {
+            string appName = "Hello World";
+            string appVersion = "1.0";
+            string appAuthor = "Mateusz Celka";
+            Console.WriteLine("{0} version: {1} by {2}",appName,appVersion,appAuthor);
         }
     }
 }
